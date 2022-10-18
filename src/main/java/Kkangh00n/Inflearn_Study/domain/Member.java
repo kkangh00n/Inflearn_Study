4d144db7e692c0e752eca08be94f383fedd4651c
kkangh00n <kig2454@gmail.com>
2022-10-18 16:04:18 +0900

    .

--- src/main/java/Kkangh00n/Inflearn_Study/domain/Member.java
+++ src/main/java/Kkangh00n/Inflearn_Study/domain/Member.java
@@ -10,24 +10,24 @@ import javax.persistence.Id;
 //도메인 (회원)
 public class Member {
 
-    @Id
+    @Id //primary key
     @GeneratedValue(strategy = GenerationType.IDENTITY) //db가 id를 알아서 생성해줌
     private Long id;
     private String name;
 
-    public Long getId() {
-        return id;
-    }
+        public Long getId() {
+            return id;
+        }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+        public void setId(Long id) {
+            this.id = id;
+        }
 
-    public String getName() {
-        return name;
-    }
+        public String getName() {
+            return name;
+        }
 
-    public void setName(String name) {
-        this.name = name;
+        public void setName(String name) {
+            this.name = name;
     }
 }
